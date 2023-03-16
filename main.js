4a45ab35a8cfbc7c02a2fab1850c3dc05832e759
Alex Chaus <chausme@gmail.com>
2023-03-16 20:00:12 +0400

    Add Bootstrap styles with layout and basic answer functionality

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log(`Hang on...it's getting closer`);
+const answerEl = document.querySelector('.js-answer');
+const btnShow = document.querySelector('.js-show');
+
+btnShow.addEventListener('click', e => {
+    if (!answerEl?.dataset?.state || answerEl?.dataset?.state === 'hidden') {
+        answerEl.classList.remove('d-none');
+        btnShow.textContent = 'Stash';
+        answerEl.dataset.state = 'visible';
+    } else {
+        answerEl.classList.add('d-none');
+        btnShow.textContent = 'Unveil';
+        answerEl.dataset.state = 'hidden';
+    }
+    e.preventDefault();
+});
